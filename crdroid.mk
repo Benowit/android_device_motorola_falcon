61c4614e13a530223850e54304cb4a4bab353820
Benowit <benowit@gmail.com>
2014-12-27 15:25:38 +0100

    delette

--- crdroid.mk
+++
@@ -1,8 +0,0 @@
-# Inherit some common crDroid stuff.
-$(call inherit-product, vendor/crdroid/config/common_full_phone.mk)
-
-# Inherit device configuration
-$(call inherit-product, device/motorola/falcon/full_falcon.mk)
-
-PRODUCT_RELEASE_NAME := MOTO G
-PRODUCT_NAME := crdroid_falcon
