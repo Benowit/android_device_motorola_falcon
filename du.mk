fd67987238d963786ccf4e24fd4fa64f4fe8ce2b
Benowit <benowit@gmail.com>
2015-10-05 15:31:53 +0200

    du'ify

--- du.mk
+++ du.mk
@@ -1,8 +1,8 @@
 # Inherit some common crDroid stuff.
-$(call inherit-product, vendor/crdroid/config/common_full_phone.mk)
+$(call inherit-product, vendor/du/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/motorola/falcon/full_falcon.mk)
 
 PRODUCT_RELEASE_NAME := MOTO G
-PRODUCT_NAME := crdroid_falcon
+PRODUCT_NAME := du_falcon
